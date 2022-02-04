d286374b2ebb8a97a9b2dd1b62dd30fafdd25a8e
shelbyrossi <rossi.shelby@gmail.com>
2022-02-04 10:56:04 -0600

    added create to views

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Event
+from levelupapi.models.game import Game
+from levelupapi.models.gamer import Gamer
 
 
 
@@ -30,6 +32,27 @@ class EventView(ViewSet):
         """
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        organizer = Gamer.objects.get(user=request.auth.user)
+        game = Game.objects.get(pk=request.data["game"])
+
+        event = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            skill_level=request.data["skill_level"],
+            game=game,
+            organizer=organizer
+        )
+        serializer = EventSerializer(event)
         return Response(serializer.data)
     
     
