3aa8b14d633b8a5714b1e1059a037061e0ebee07
shelbyrossi <rossi.shelby@gmail.com>
2022-02-23 13:32:02 -0600

    added report module

--- levelup/urls.py
+++ levelup/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('', include('levelupreports.urls')),
 ]
